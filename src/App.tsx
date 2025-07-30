fad22274b153dd6e9102ae86cf952940f7f533ae
Flappy <stvnsbarrantes@gmail.com>
2025-07-31 03:13:54 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { Outlet } from "@tanstack/react-router";
-import { useGitProd } from "@_/hooks/useGitProd";
+import { useGitProd, useSkills, useSoftSkill } from "@_/hooks/useGitProd";
+import { useEducation, useExperience } from "@_/hooks/useOthers";
 import { useDarkMode } from "@_/stores/useDarkMode";
 import LoadingText from "@_/shared/Loading/index";
 import "./App.css";
 
 function App() {
   const { isDarkMode } = useDarkMode();
-  const { isLoading, data: gitProd} = useGitProd("All", 1);
-
-  if (isLoading || !gitProd) {
+  const { isLoading, data: gitProd } = useGitProd("All", 1);
+  const { data: Educ } = useEducation();
+  const { data: Exp } = useExperience();
+  const { data: skills } = useSkills();
+  const { data: softSkill } = useSoftSkill();
+  if (isLoading || !gitProd || !skills || !softSkill || !Educ || !Exp) {
     return (
       <div
         className={`flex items-center justify-center h-screen p-4 ${isDarkMode ? "bg-black" : "bg-white"}`}
